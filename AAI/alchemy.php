a0aa115775b81691366eb89d5cb1ffb697e06800
Felix <git@pwn.it>
2014-11-20 10:21:43 +0100

    image example in alchemy

--- AAI/alchemy.php
+++ AAI/alchemy.php
@@ -2,12 +2,20 @@
 
 /**
  * Test of the Alchemy API
- * http://www.alchemyapi.com/developers/getting-started-guide/using-alchemyapi-with-php
+ * Doku under:
+ * https://github.com/AlchemyAPI/alchemyapi_php
  */
 
 require_once '../alchemyapi_php/alchemyapi.php';
 $alchemyapi = new AlchemyAPI();
 
+
 $myText = "I can't wait to integrate AlchemyAPI's awesome PHP SDK into my app!";
 $response = $alchemyapi->sentiment("text", $myText, null);
 echo "Sentiment: ", $response["docSentiment"]["type"], PHP_EOL;
+
+$response = $alchemyapi->image_keywords('url','https://c2.staticflickr.com/6/5531/9629720567_f4a95951bb_z.jpg', null);
+foreach ($response['imageKeywords'] as $imagekeyword) {
+    echo 'keyword: ', $imagekeyword['text'], PHP_EOL;
+    echo 'score: ', $imagekeyword['score'], PHP_EOL;
+}
