89ae1924f9f219735dc8a003abd70629477f7a98
Felix <git@pwn.it>
2014-11-20 18:12:07 +0100

    typo

--- RTS/RMS.js
+++ RTS/RMS.js
@@ -14,20 +14,180 @@ angular.module('myApp.RTS', ['ngRoute'])
                         'a 3 1 3\n'+
                         'b 6 1 6\n'+
                         'c 5 1 5\n'+
-                        'd 10 2 10 ';
+                        'd 10 3 10 ';
 
         $scope.$watch('table', function(newValue, oldValue) {
-            var threads = $scope.getThreadsFromRaw(newValue);
-
+            var threads         = $scope.getThreadsFromRaw(newValue);
             $scope.threadsRMS   = $scope.calculate(threads, 'RMS');
             $scope.tableRMS     = $scope.calculateTable($scope.threadsRMS);
 
+            var threads         = $scope.getThreadsFromRaw(newValue);
             $scope.threadsDMS   = $scope.calculate(threads, 'DMS');
             $scope.tableDMS     = $scope.calculateTable($scope.threadsDMS);
 
+            var threads         = $scope.getThreadsFromRaw(newValue);
             $scope.threadsRTA   = $scope.calculate(threads, 'RTA');
             $scope.tableRTA     = $scope.calculateTable($scope.threadsRTA);
         });
+
+        $scope.getThreadsFromRaw = function(rawData) {
+            var re = /([a-z]?)\s+([0-9]{1,3})\s+([0-9]{1,3})\s?([0-9]{1,3})?/gim;
+            var str = rawData;
+            var m;
+            var data = [];
+
+            while ((m = re.exec(str)) != null) {
+                if (m.index === re.lastIndex) {
+                    re.lastIndex++;
+                }
+
+                var D = m[4] || m[2];
+                data.push({
+                    name: m[1],
+                    T: parseInt(m[2]),
+                    C: parseInt(m[3]),
+                    D: parseInt(D),
+                    P: 0,
+                    suspended: 0,
+                    runcount: 0,
+                    cycluscount: 0
+                });
+            }
+            return data;
+        }
+
+        $scope.isFeasible = function(thread) {
+            console.log('isFeasible', thread.complexRTA, thread.D);
+            if(thread.complexRTA>thread.D) return false;
+            else return true;
+        }
+
+        $scope.optimalPriorityAssignment = function(threads) {
+            var N = threads.length;
+            for(var K=1; K<N; K++) {
+                for(var Next=K; Next<N; Next++) {
+                    console.log("swap", Next, K);
+                    var tmp = threads[K];
+                    threads[K] = threads[Next];
+                    threads[Next] = tmp;
+
+                    /*threads.sort(function (a, b) {
+                        return (a.P - b.P)
+                    });*/
+                    //$scope.calculateSimpleRTA(threads);
+                    //$scope.calculateComplexRTA(threads);
+
+                    var ok = $scope.isFeasible(threads[K]);
+                    console.log('isFeasible', ok, K, threads[K]);
+                    if(ok) break;
+
+                }
+                if(!ok) {
+                    console.error('no RTA found');
+                    return false;
+                }
+            }
+        }
+
+        $scope.calculate = function(data, scheduler) {
+            if(scheduler=='DMS') {
+                data.sort(function (a, b) {
+                    return (a.D - b.D)
+                });
+            }else if(scheduler=='RTA') {
+                $scope.calculateSimpleRTA(data);
+                $scope.calculateComplexRTA(data);
+
+                $scope.optimalPriorityAssignment(data);
+            }else{
+                data.sort(function (a, b) {
+                    return (a.T - b.T)
+                });
+            }
+
+            $scope.utilization = 0;
+            var priority=data.length;
+            $scope.LCM = Math.abs(data[0].T);
+            data.forEach(function(entry) {
+                entry.P = priority;
+                priority--;
+
+                entry.utilization = entry.C/entry.T;
+                $scope.utilization += entry.utilization;
+
+                var b = Math.abs(entry.T), c = $scope.LCM;
+                while ($scope.LCM && b){ $scope.LCM > b ? $scope.LCM %= b : b %= $scope.LCM; }
+                $scope.LCM = Math.abs(c*entry.T)/($scope.LCM+b);
+            });
+            $scope.maxUtilization = data.length * (Math.pow(2, 1/data.length)-1)
+
+            return data; //return a copy!
+        }
+
+        $scope.calculateTable = function(threads) {
+            var table = [];
+            var threadIndex = 0;
+            threads.forEach(function(thread) {
+                for(var cyclus=0;cyclus<$scope.LCM;cyclus++) {
+                    table[threadIndex] = table[threadIndex] || [];
+
+                    var blocked = $scope.isThreadBlocked(threads, table, threadIndex-1, cyclus);
+                    var self = (cyclus%thread.T)<=thread.T //new Period T
+                        && (cyclus%thread.T)+1<=thread.C; //Computation C not elapsed
+
+                    if(self && blocked) {
+                        thread.suspended++;
+                    }else if(!self && !blocked && thread.suspended>0){
+                        thread.suspended--;
+                        self = true;
+                    }
+                    if(self && !blocked) {
+                        if(thread.C == 1) {
+                            table[threadIndex][cyclus] = {running: true, symbol: '<>', class:'startend'};
+                            thread.cycluscount++;
+                        }else if (thread.runcount == 0) {
+                            table[threadIndex][cyclus] = {running: true, symbol: '<', class:'start'};
+                            thread.runcount++;
+                            thread.cycluscount++;
+                        } else if (thread.runcount == thread.C-1) {
+                            table[threadIndex][cyclus] = {running: true, symbol: '>', class:'end'};
+                            thread.runcount = 0;
+                        } else {
+                            table[threadIndex][cyclus] = {running: true, symbol: thread.runcount+1, class:'running'};
+                            thread.runcount++;
+                        }
+
+                        //var cyclusCount = Math.ceil(cyclus/thread.D);
+
+                        if(cyclus>=thread.cycluscount*(thread.D)) {
+                            table[threadIndex][cyclus].class = table[threadIndex][cyclus].class+' deadline';
+                        }
+                    }
+                    else{
+                        table[threadIndex][cyclus] = {running: false, symbol: '', class:''};
+                    }
+                };
+                threadIndex++;
+            });
+
+            return table;
+        }
+
+        $scope.isThreadRunning = function(running, thread, threadIndex, cyclus) {
+            return running[threadIndex][cyclus];
+        }
+
+        $scope.isThreadBlocked = function(threads, running, threadIndex, cyclus) {
+            if(threadIndex < 0 ) return false; //Ready
+            var parent = $scope.isThreadBlocked(threads, running, threadIndex-1, cyclus); //higher prio Task not running
+
+            if(running[threadIndex][cyclus].running) return true || parent;
+            else return false || parent;
+        }
+
+        $scope.getNumber = function(num) {
+            return new Array(num);
+        }
 
         $scope.calculateSimpleRTA = function(threads) {
             $scope.debugSimpleRTA = '';
@@ -99,161 +259,7 @@ angular.module('myApp.RTS', ['ngRoute'])
             return responseTime;
         }
 
-        $scope.isFeasible = function(thread) {
-            console.log('isFeasible', thread.complexRTA, thread.D);
-            if(thread.complexRTA>thread.D) return false;
-            else return true;
-        }
-
-        $scope.optimalPriorityAssignment = function(threads) {
-            var N = threads.length;
-            for(var K=1; K<N; N++) {
-                for(var Next=K; Next<N; Next++) {
-                    console.log("swap", Next, K);
-                    var tmp = threads[K];
-                    threads[K] = threads[Next];
-                    threads[Next] = tmp;
-
-                    var ok = $scope.isFeasible(threads[K]);
-                    console.log('isFeasible', K, threads[K]);
-                    if(ok) break;
-
-                }
-                if(!ok) console.error('no RTA found');
-                return false;
-            }
-
-        }
-
-        $scope.getThreadsFromRaw = function(rawData) {
-            var re = /([a-z]?)\s+([0-9]{1,3})\s+([0-9]{1,3})\s?([0-9]{1,3})?/gim;
-            var str = rawData;
-            var m;
-            var data = [];
-
-            while ((m = re.exec(str)) != null) {
-                if (m.index === re.lastIndex) {
-                    re.lastIndex++;
-                }
-
-                var D = m[4] || m[2];
-                var priority = 1;
-                data.push({
-                    name: m[1],
-                    T: parseInt(m[2]),
-                    C: parseInt(m[3]),
-                    D: parseInt(D),
-                    P: priority++,
-                    suspended: 0,
-                    runcount: 0,
-                    cycluscount: 0
-                });
-            }
-            return data;
-        }
-        $scope.calculate = function(data, scheduler) {
-            if(scheduler=='DMS') {
-                data.sort(function (a, b) {
-                    return (a.D - b.D)
-                });
-            }else if(scheduler=='RTA') {
-                $scope.calculateSimpleRTA(data);
-                $scope.calculateComplexRTA(data);
-
-                $scope.optimalPriorityAssignment(data);
-                data.sort(function (a, b) {
-                    return (a.P - b.P)
-                });
-            }else{
-                data.sort(function (a, b) {
-                    return (a.T - b.T)
-                });
-            }
-
-            $scope.utilization = 0;
-            var priority=1;
-            $scope.LCM = Math.abs(data[0].T);
-            data.forEach(function(entry) {
-                entry.P = priority;
-                priority++;
-
-                entry.utilization = entry.C/entry.T;
-                $scope.utilization += entry.utilization;
-
-                var b = Math.abs(entry.T), c = $scope.LCM;
-                while ($scope.LCM && b){ $scope.LCM > b ? $scope.LCM %= b : b %= $scope.LCM; }
-                $scope.LCM = Math.abs(c*entry.T)/($scope.LCM+b);
-            });
-            $scope.maxUtilization = data.length * (Math.pow(2, 1/data.length)-1)
-
-            return data.slice(); //return a copy!
-        }
-
-        $scope.calculateTable = function(threads) {
-            var table = [];
-            var threadIndex = 0;
-            threads.forEach(function(thread) {
-                for(var cyclus=0;cyclus<$scope.LCM;cyclus++) {
-                    table[threadIndex] = table[threadIndex] || [];
-
-                    var blocked = $scope.isThreadBlocked(threads, table, threadIndex-1, cyclus);
-                    var self = (cyclus%thread.T)<=thread.T //new Period T
-                        && (cyclus%thread.T)+1<=thread.C; //Computation C not elapsed
-
-                    if(self && blocked) {
-                        thread.suspended++;
-                    }else if(!self && !blocked && thread.suspended>0){
-                        thread.suspended--;
-                        self = true;
-                    }
-                    if(self && !blocked) {
-                        if(thread.C == 1) {
-                            table[threadIndex][cyclus] = {running: true, symbol: '<>', class:'startend'};
-                            thread.cycluscount++;
-                        }else if (thread.runcount == 0) {
-                            table[threadIndex][cyclus] = {running: true, symbol: '<', class:'start'};
-                            thread.runcount++;
-                            thread.cycluscount++;
-                        } else if (thread.runcount == thread.C-1) {
-                            table[threadIndex][cyclus] = {running: true, symbol: '>', class:'end'};
-                            thread.runcount = 0;
-                        } else {
-                            table[threadIndex][cyclus] = {running: true, symbol: thread.runcount+1, class:'running'};
-                            thread.runcount++;
-                        }
-
-                        //var cyclusCount = Math.ceil(cyclus/thread.D);
-
-                        if(cyclus>=thread.cycluscount*(thread.D)) {
-                            table[threadIndex][cyclus].class = table[threadIndex][cyclus].class+' deadline';
-                        }
-                    }
-                    else{
-                        table[threadIndex][cyclus] = {running: false, symbol: '', class:''};
-                    }
-                };
-                threadIndex++;
-            });
-
-            return table;
-        }
-
-        $scope.isThreadRunning = function(running, thread, threadIndex, cyclus) {
-            return running[threadIndex][cyclus];
-        }
-
-        $scope.isThreadBlocked = function(threads, running, threadIndex, cyclus) {
-            if(threadIndex < 0 ) return false; //Ready
-            var parent = $scope.isThreadBlocked(threads, running, threadIndex-1, cyclus); //higher prio Task not running
-
-            if(running[threadIndex][cyclus].running) return true || parent;
-            else return false || parent;
-        }
-
-        $scope.getNumber = function(num) {
-            return new Array(num);
-        }
-}]);
+    }]);
 
 
 function LCM(A)  // A is an integer array (e.g. [-50,25,-45,-18,90,447])
